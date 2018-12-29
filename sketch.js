6050144bde593e2d26b11a59797ab2d007e5e1fb
Vincent <vincentvanmontfoort@gmail.com>
2018-12-29 23:21:04 +0100

    set max

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var n = 0;
 var c = 8;
+var up = true;
 
 function setup() {
     createCanvas(750, 750);
@@ -9,14 +10,16 @@ function setup() {
 }
 
 function draw() {
-    var a = n * 137.3;
+    var a = n * 137.5;
     var r = c * sqrt(n);
 
     var x = r * cos(a) + width / 2;
     var y = r * sin(a) + height / 2;
-    fill(a % 256, n % 255,255);
+    fill(a % 256, n % 256, c * 255);
     ellipse(x, y, 12, 12);
     noStroke();
     n++
-
+    if(n === 4100){
+        n = 0;
+    }
 }
