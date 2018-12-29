dd5c7fd68d3e6390b2bdfd3aa274c17870cc2f40
Vincent <vincentvanmontfoort@gmail.com>
2018-12-29 23:31:30 +0100

    rest of initial

--- sketch.js
+++ sketch.js
@@ -1,25 +1,9 @@
-var n = 0;
-var c = 8;
-var up = true;
+
 
 function setup() {
-    createCanvas(750, 750);
-    angleMode(DEGREES)
-    colorMode(HSB)
-    background(0);
+
 }
 
 function draw() {
-    var a = n * 137.5;
-    var r = c * sqrt(n);
 
-    var x = r * cos(a) + width / 2;
-    var y = r * sin(a) + height / 2;
-    fill(a % 256, n % 256, c * 255);
-    ellipse(x, y, 12, 12);
-    noStroke();
-    n++
-    if(n === 4100){
-        n = 0;
-    }
 }
