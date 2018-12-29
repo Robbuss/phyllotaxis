57d4f5f0332f2745390e26cc3becd9a50e654c3e
Vincent <vincentvanmontfoort@gmail.com>
2018-12-29 23:36:23 +0100

    oops

--- sketch.js
+++ sketch.js
@@ -1,9 +1,24 @@
-
+var n = 0;
+var c = 8;
 
 function setup() {
-
+    createCanvas(750, 750);
+    angleMode(DEGREES)
+    colorMode(HSB)
+    background(0);
 }
 
 function draw() {
+    var a = n * 137.3;
+    var r = c * sqrt(n);
 
+    var x = r * cos(a) + width / 2;
+    var y = r * sin(a) + height / 2;
+    fill(a % 256, n % 255, 255);
+    ellipse(x, y, 12, 12);
+    noStroke();
+    n++	
+    if(n === 4100){	
+        n = 0;	
+    }
 }
